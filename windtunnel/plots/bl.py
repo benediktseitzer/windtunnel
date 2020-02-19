bug fixes several small bug fixes for bl, timeseries and timeseries_nc changes to timeseries_nc to prepare for the integration of a nc spectra calculation and plotting

windtunnel/plots/bl.py
@@ -5,6 +5,8 @@ import numpy as np
 from scipy import signal
 import windtunnel as wt
 
+plt.rcParams["figure.figsize"] = (9,6)
+plt.rcParams.update({'font.size': 15})
 
 __all__ = [
     'plot_scatter',
@@ -124,17 +126,19 @@ def plot_scatter_wght(transit_time,x,y,std_mask=5.,ax=None,**kwargs):
 
     
 def plot_hist(data,ax=None,**kwargs):
-    """Creates a historgram plot of x and y.
+    """Creates a scatter plot of x and y.
     @parameter: data, type = list or np.array
     @parameter ax: axis passed to function
     @parameter kwargs : additional keyword arguments passed to plt.plot() """
-    #edit 08/12/2019: Corrected program description to specify that function plots histogram, not scatter plot. 
+	#edit 08/01/2019: convert data from times series to array manually, to avoid
+    #compatibility issues with python 3.6 & pandas 0.24
     
     # Get current axis
     if ax is None:
        ax = plt.gca()
        
     #  Calculate bin size and normalise count
+    data=np.asarray(data)
     count,bins = np.histogram(data[~np.isnan(data)],
                               bins=np.linspace(np.nanmin(data),np.nanmax(data),
                               np.max([np.nanmin([25,(int(np.nanmax(data)-
@@ -424,6 +428,7 @@ def plot_lux(Lux, heights, err=0, lat=False, ref_path=None, ax=None,
     @parameter: ref_path = string
     @parameter ax: axis passed to function
     @parameter kwargs : additional keyword arguments passed to plt.plot() """
+	#edit 08/02/2019: moved labels to ax.plot, to ensure proper plotting of legend, and removing need for extra labels variable.
     if ax is None:
        ax = plt.gca()
 
@@ -433,29 +438,31 @@ def plot_lux(Lux, heights, err=0, lat=False, ref_path=None, ax=None,
 
     ret = []
     if lat == False:
-        Lux = ax.errorbar(Lux,heights,xerr=err,fmt='o',color='navy',)
-        ref1 = ax.plot(Lux_10[1,:],Lux_10[0,:],'k-',linewidth=1)
-        ref2 = ax.plot(Lux_1[1,:],Lux_1[0,:],'k--',linewidth=1)
-        ref3 = ax.plot(Lux_01[1,:],Lux_01[0,:],'k-.',linewidth=1)
-        ref4 = ax.plot(Lux_001[1,:],Lux_001[0,:],'k:',linewidth=1,)
+        Lux = ax.errorbar(Lux,heights,xerr=err,fmt='o',color='navy',label='wind tunnel')
+        ref1 = ax.plot(Lux_10[1,:],Lux_10[0,:],'k-',linewidth=1,label=r'$z_0=10\ m$ (theory)')
+        ref2 = ax.plot(Lux_1[1,:],Lux_1[0,:],'k--',linewidth=1,label=r'$z_0=1\ m$ (theory)')
+        ref3 = ax.plot(Lux_01[1,:],Lux_01[0,:],'k-.',linewidth=1,label=r'$z_0=0.1\ m$ (theory)')
+        ref4 = ax.plot(Lux_001[1,:],Lux_001[0,:],'k:',linewidth=1,label=r'$z_0=0.01\ m$ (theory)')
         ref5 = ax.plot(Lux_obs_smooth[1,:],Lux_obs_smooth[0,:],'k+',
-                       linewidth=1)
-        ref6 = ax.plot(Lux_obs_rough[1,:],Lux_obs_rough[0,:],'kx',linewidth=1)
+                       linewidth=1,label='observations smooth surface')
+        ref6 = ax.plot(Lux_obs_rough[1,:],Lux_obs_rough[0,:],'kx',linewidth=1,label='observations rough surface')
     
-        labels = ['wind tunnel',r'$z_0=10\ m$ (theory)',r'$z_0=1\ m$ (theory)',
-                  r'$z_0=0.1\ m$ (theory)',r'$z_0=0.01\ m$ (theory)',
-                  'observations smooth surface','observations rough surface']
+        #labels = ['wind tunnel',r'$z_0$=10\ m$ (theory)',r'$z_0=1\ m$ (theory)',
+                  #r'$z_0=0.1\ m$ (theory)',r'$z_0=0.01\ m$ (theory)',
+                  #'observations smooth surface','observations rough surface']
         
+
         ax.set_yscale('log')
         ax.set_xscale('log')
         ax.grid(True,'both','both')
     
-        ax.legend([Lux,ref1,ref2,ref3,ref4,ref5,ref6],labels,
-                  bbox_to_anchor=(0.5,1.05),loc='lower center',
-                  borderaxespad=0.,ncol=2,fontsize=16)
+        #ax.legend([Lux,ref1,ref2,ref3,ref4,ref5,ref6],labels,
+                  #bbox_to_anchor=(0.5,1.05),loc='lower center',
+                  #borderaxespad=0.,ncol=2,fontsize=16)
+        ax.legend(loc='upper center',bbox_to_anchor=(0.5,1.5),borderaxespad=0.,ncol=2,fontsize=12)
         
         ax.set_xlim([10,1000])
-        ax.set_ylim([10,1000])
+        ax.set_ylim([min(heights),1000])
         ax.set_xlabel(r'$L_{u}^{x}$ full-scale [m]')
         ax.set_ylabel(r'$z$ full-scale [m]')    
         
@@ -463,7 +470,7 @@ def plot_lux(Lux, heights, err=0, lat=False, ref_path=None, ax=None,
         Lux = ax.errorbar(heights,Lux,yerr=err,fmt='o',color='navy')
         labels = ['wind tunnel']
         ax.grid(True)
-        ax.legend([Lux],labels,bbox_to_anchor=(0.5,1.05),loc='lower center',
+        ax.legend([Lux],labels,bbox_to_anchor=(0.5,1.05),loc='upper center',
                   borderaxespad=0.,ncol=2,fontsize=16)
         ax.set_xlabel(r'$z$ full-scale [m]')
         ax.set_ylabel(r'$L_{u}^{x}$ full-scale [m]')    
@@ -488,7 +495,8 @@ def plot_spectra(f_sm, S_uu_sm, S_vv_sm, S_uv_sm, u_aliasing, v_aliasing,
 #    xsmax = np.nanmax(100,np.nanmax(f_sm[np.where(f_sm>0)]))
     ref_x = np.logspace(np.log10(xsmin),np.log10(xsmax),50)
     ref_specs = wt.get_reference_spectra(height,ref_path)
-        
+    
+    print('Spectra')    
     h1 = ax.loglog(f_sm[:u_aliasing],S_uu_sm[:u_aliasing],'ro',markersize=3,
                label=r'wind tunnel $'+'{0}{0}'.format(wind_comps[0])+'$')
     h2 = ax.loglog(f_sm[u_aliasing:],S_uu_sm[u_aliasing:],'ro',markersize=3,
@@ -526,7 +534,7 @@ def plot_spectra(f_sm, S_uu_sm, S_vv_sm, S_uv_sm, u_aliasing, v_aliasing,
     return h1,h2,h3,h4
 
 
-def plot_Re_independence(data,wtref,yerr=0,ax=None,**kwargs):
+def plot_Re_independence(data,wtref,ymin=None,ymax=None,yerr=0,ax=None,**kwargs):
     """ Plots the results for a Reynolds Number Independence test from a non-
     dimensionalised timeseries. yerr specifies the uncertainty. Its default 
     value is 0.
@@ -537,7 +545,10 @@ def plot_Re_independence(data,wtref,yerr=0,ax=None,**kwargs):
     @parameter: kwargs: additional keyword arguments passed to plt.plot()"""
     if ax is None:
         ax=plt.gca()
-
+    if ymin is None:
+       ymin=np.min(data)
+    if ymax is None:
+       ymax=np.max(data)
     # Sort wtref and data to correspond to increasing wtref values
     data = [wtref for _,wtref in sorted(zip(wtref,data))]
     wtref = sorted(wtref)
@@ -547,6 +558,7 @@ def plot_Re_independence(data,wtref,yerr=0,ax=None,**kwargs):
     for i,value in enumerate(data):
         l = ax.errorbar(wtref[i],value,yerr=yerr,fmt='o',markersize=4,
                         ls='None',color='navy',**kwargs)
+        ax.set_ylim((ymin,ymax))
         ret.append(l)
         
     ax.set_xlabel(r'$U_{0}$ $[ms^{-1}]$')
@@ -557,7 +569,7 @@ def plot_Re_independence(data,wtref,yerr=0,ax=None,**kwargs):
     return ret
 
 
-def plot_convergence_test(data,wtref=1,ref_length=1,scale=1,ylabel='',ax=None,
+def plot_convergence_test(data,wtref=1,ref_length=1,scale=1,ylabel='',title='',ax=None,
                           **kwargs):
     """Plots results of convergence tests  from data. This is a very limited 
     function and is only intended to give a brief overview of the convergence
@@ -573,26 +585,28 @@ def plot_convergence_test(data,wtref=1,ref_length=1,scale=1,ylabel='',ax=None,
 
     if ax is None:
         ax = plt.gca()
-    
-    handles = []   
-    keys = [key for key in data.keys()]
-    keys.sort()
-    for i, key in enumerate(keys):
-        l, = ax.plot([i] * len(data[key]), data[key], color='navy',
-                      linestyle='None',marker='o', markersize=15)                  
+    handles = []
+    print(['ylabel ='+ylabel])
+    for i, key in enumerate([key for key in sorted(data.keys())]):
+        l, = ax.plot([key]*len(data[key]), data[key], color='navy',
+        #l, = ax.plot(key, data[key], color='navy',
+		linestyle='None',marker='o', markersize=15) 		  
         ax.grid(True)
+        ax.set_title(title)
+        ax.set_ylabel(ylabel)
+        ax.set_xlabel('Interval Size')
         handles.append(l)
     
-    xticklabels=[key for key in data.keys()]
-    xticklabels=[int((x*wtref/ref_length)/scale) for x in xticklabels]
-    ax.set(xticks=np.arange(0,len(data.keys())+1),
-                  xticklabels=xticklabels[::10],
-                  xlim=(-0.5, len(data.keys())-0.5))
-    ax.locator_params(axis='x', nbins=10)
-    ax.tick_params(labelsize=12)
-    ax.set_ylabel(ylabel, fontsize=18)
-    ax.set_xlabel(r'$\Delta t(wind\ tunnel)\cdot U_{0}\cdot L_{0}^{-1}$',
-                  fontsize=18)
+    #xticklabels=[key for key in data.keys()]
+    #xticklabels=[int((x*wtref/ref_length)/scale) for x in xticklabels]
+    #ax.set(xticks=np.arange(0,len(data.keys())+1),
+                  #xticklabels=xticklabels,
+                  #xlim=(-0.5, len(data.keys())-0.5))
+    #ax.locator_params(axis='x', nbins=10)
+    #ax.tick_params(labelsize=12)
+    #ax.set_ylabel(ylabel, fontsize=18)
+    #ax.set_xlabel(r'$\Delta t(wind\ tunnel)\cdot U_{0}\cdot L_{0}^{-1}$',
+                  #fontsize=18)
 
     return handles
     
@@ -606,11 +620,11 @@ def plot_convergence(data_dict,ncols=3,**kwargs):
     @parameter: data_dict, type = dictionary
     @parameter: ncols, type = int
     @parameter: kwargs keyword arguments passed to plot_convergence_test"""
-    
+
     fig, axes = plt.subplots(ncols,int(np.ceil(len(data_dict.keys())/ncols)),
                              figsize=(24,14))
     for (key,data), ax in zip(data_dict.items(), axes.flat):
-        plot_convergence_test(data,ylabel=key,ax=ax,**kwargs)        
+        plot_convergence_test(data,ylabel=key,ax=ax,**kwargs)
 
     return axes
 

windtunnel/timeseries.py
@@ -23,7 +23,7 @@ class Timeseries(pd.DataFrame):
     @parameter: t_arr, type = np.array
     @parameter: t_transit, type = np.array
     @parameter: tau, type = int or float - time scale in milliseconds"""
-    def __init__(self,u,v,x=None,y=None,z=None,t_arr=None,t_transit=None,
+    def __init__(self,u,u_eq,v,v_eq,x=None,y=None,z=None,t_arr=None,t_transit=None,
                  tau=10000):
         """ Initialise Timerseries() object. """
         super().__init__()
@@ -33,8 +33,10 @@ class Timeseries(pd.DataFrame):
         self.t_arr = t_arr
         self.t_transit = t_transit
         self['u'] = u
+        self['u_eq'] = u_eq
         self.u_unmasked = u
         self['v'] = v
+        self['v_eq'] = v_eq
         self.v_unmasked = v
         self.tau = tau # time scale in milliseconds
         self.weighted_u_mean = None
@@ -43,7 +45,9 @@ class Timeseries(pd.DataFrame):
         self.weighted_comp_2_var = None
         self.scale = None
         self.wtref = None
-        self.t_eq = None
+        self.t_eq = None 
+        self.u_eq = None  
+        self.v_eq = None        
         self.magnitude = None
         self.direction = None
         self.u1 = None
@@ -62,7 +66,7 @@ class Timeseries(pd.DataFrame):
 
     @classmethod
     def from_file(cls,filename):
-        """ Create Timeseries object from file. """
+        """ Create Timeseries object from file."""
         with open(filename) as file:
             for i, line in enumerate(file):
                 if i == 3:
@@ -72,12 +76,17 @@ class Timeseries(pd.DataFrame):
                     break
 
         t_arr, t_transit, u, v = np.genfromtxt(filename,usecols=(1,2,3,4),
-                                                skip_header=6,unpack=True)											   
+                                               skip_header=6,unpack=True)
 
-        ret = cls(u,v,x,y,z,t_arr,t_transit)
-
-        ret.calc_equidistant_timesteps()
-
+        #edit 06/20/19: create dummy variables for equidistant time series
+        u_eq=u/u
+        v_eq=v/v
+        
+        
+        ret = cls(u,u_eq,v,v_eq,x,y,z,t_arr,t_transit)
+        #edit 6/20/19: want to make time series equidistant in seperate function            
+        #ret.calc_equidistant_timesteps()  
+   
         return ret
 
     def get_wtref(self,wtref_path,filename,index=0,vscale=1.):
@@ -96,6 +105,8 @@ class Timeseries(pd.DataFrame):
             all_wtrefs = np.genfromtxt(wtreffile,usecols=(3),skip_header=1)
         except OSError:
             print(' ATTENTION: wtref-file not found at ' + wtreffile + '!')
+            
+            
 
         if np.size(all_wtrefs) == 1:
             self.wtref = float(all_wtrefs) * vscale
@@ -110,6 +121,7 @@ class Timeseries(pd.DataFrame):
                 if i == 5:
                     self.wind_comp1 = line.split()[-4][-1].lower()
                     self.wind_comp2 = line.split()[-2][-1].lower()
+                    break
 
     def nondimensionalise(self):
         """ Nondimensionalise the data. wtref is set to 1 if no wtref is
@@ -130,35 +142,43 @@ class Timeseries(pd.DataFrame):
         self.y = self.y * self.scale/1000           #[m]
         self.z = self.z * self.scale/1000           #[m]
         self.t_arr = self.t_arr * self.scale/1000   #[s]
+        #edit 06/20/19: t_eq now generated after adapt_scale
+        #self.t_eq = self.t_eq * self.scale/1000   #[s]        
 
     def calc_equidistant_timesteps(self):
         """ Create equidistant time series. """
         self.t_eq = np.linspace(self.t_arr[0],self.t_arr[-1],len(self.t_arr))
-        self.u[:] = wt.equ_dist_ts(self.t_arr,self.t_eq,self.u.values)
-        self.v[:] = wt.equ_dist_ts(self.t_arr,self.t_eq,self.v.values)
+        self.u_eq = wt.equ_dist_ts(self.t_arr,self.t_eq,self.u.values)
+        self.v_eq = wt.equ_dist_ts(self.t_arr,self.t_eq,self.v.values)
         
         self.index = self.t_eq
+        
 
-    def mask_outliers(self,std_mask=5.):
+    def mask_outliers(self,std_mask=5):
         """ Mask outliers and print number of outliers. std_mask specifies the
         threshold for a value to be considered an outlier. 5 is the default
         value for std_mask.
         @parameter: std_mask, type = float"""
+        #edit 6/12/19: added variables n_outliers_u and n_outliers_v to keep track of the number of outliers
+        #edit 4/7/19: revised procedure for handling outliers. Outliers are npw set to nan by default.
         u_size = np.size(self.u)
         v_size = np.size(self.v)
-
+        
         # Mask outliers
-        u_mask = np.asarray(self.u)<(std_mask*np.std(np.asarray(self.u))+
-                            np.mean(np.asarray(self.u)))
-        v_mask = np.asarray(self.v)<(std_mask*np.std(np.asarray(self.v))+
-                            np.mean(np.asarray(self.v)))
+        u_mask = np.abs(np.asarray(self.u) - np.mean(np.asarray(self.u))) < \
+                 (std_mask*np.std(np.asarray(self.u)))
+        v_mask = np.abs(np.asarray(self.v) - np.mean(np.asarray(self.v))) < \
+                 (std_mask*np.std(np.asarray(self.v)))
+                 
+                 
         mask = np.logical_and(u_mask, v_mask)
-
         self.u = self.u[mask]
         self.v = self.v[mask]
-        self.t_transit = self.t_transit[mask]
-        self.t_arr = self.t_arr[mask]
-        self.t_eq = self.t_eq[mask]
+        self.u_eq = self.u_eq[mask]
+        self.v_eq = self.v_eq[mask]
+        self.t_transit[mask==False] = np.nan
+        self.t_arr[mask==False] = np.nan
+
 
         # Log outliers in console and to file
         logger.info('Outliers component 1: {} or {:.4f}%'.format(
@@ -169,7 +189,107 @@ class Timeseries(pd.DataFrame):
             np.size(np.where(~v_mask)),
             np.size(np.where(~v_mask))/v_size*100
         ))
+        
+        self.n_outliers_u=np.size(np.where(~u_mask))
+        self.n_outliers_v=np.size(np.where(~v_mask))
+        
+    def mask_outliers_wght(self, std_mask=5.):
+        """ Mask outliers and print number of outliers. std_mask specifies the
+        threshold for a value to be considered an outlier. 5 is the default 
+        value for std_mask. This function uses time transit time weighted 
+        statistics.
+        @parameter: std_mask, type = float"""
+        
+        u_size = np.size(self.u)
+        v_size = np.size(self.v)
+        
+        u_mean_wght = wt.transit_time_weighted_mean(self.t_transit, 
+                                                    np.asarray(self.u))
+        u_std_wght = np.sqrt(wt.transit_time_weighted_var(self.t_transit, 
+                                                          np.asarray(self.u)))
+        v_mean_wght = wt.transit_time_weighted_mean(self.t_transit, 
+                                                    np.asarray(self.v))
+        v_std_wght = np.sqrt(wt.transit_time_weighted_var(self.t_transit, 
+                                                          np.asarray(self.v)))
+        
+        # Mask outliers
+        u_mask = np.abs(np.asarray(self.u) - u_mean_wght) < \
+                 (std_mask * u_std_wght)
+        v_mask = np.abs(np.asarray(self.v) - v_mean_wght) < \
+                 (std_mask * v_std_wght)
+        
+        mask = np.logical_and(u_mask, v_mask)
+        
+        self.u = self.u[mask]
+        self.v = self.v[mask]
+        self.u_eq = self.u_eq[mask]
+        self.v_eq = self.v_eq[mask]
+        self.t_transit = self.t_transit[mask]
+        self.t_arr = self.t_arr[mask]
+        self.t_eq = self.t_eq[mask]
+        
+        # Log outliers in console and to file
+        logger.info('Outliers component 1: {} or {:.4f}%'.format(
+            np.size(np.where(~u_mask)),
+            np.size(np.where(~u_mask)) / u_size * 100
+        ))
+        logger.info('Outliers component 2: {} or {:.4f}%'.format(
+            np.size(np.where(~v_mask)),
+            np.size(np.where(~v_mask)) / v_size * 100
+        ))
+        
 
+    def tilt_coords(self):
+        """ Tilt the coordinate system so that the x-axis is always parallel to
+        the local mean wind direction. The y-axis stays horizontal while being 
+        perpendicular to the x-axis while the z-axis is perpendicular to the 
+        tilted x- and the tilted y-axis."""
+        
+        u_mean=np.nanmean(np.asarray(self.u))
+        v_mean=np.nanmean(np.asarray(self.v))
+        self.tilt_angle_deg=np.arctan2(v_mean,u_mean)*180/np.pi        
+
+        self.u_cart = self.u.copy()
+        self.v_cart = self.v.copy()
+        
+        fac1=1/np.sqrt(1+v_mean**2/u_mean**2)
+        
+		
+        u_tiltcorr = self.u*fac1 + self.v*fac1*v_mean/u_mean
+        v_tiltcorr = -self.u*fac1*v_mean/u_mean + self.v*fac1
+        
+        self.u=u_tiltcorr
+        self.v=v_tiltcorr
+
+        #"""Alternate Method:Represent u and v by a single set of complex numbers.""" 
+
+        #u_complex=self.u+1j*self.v
+        #u_complex=np.exp(-1j*self.tilt_angle_deg*np.pi/180)*u_complex
+		
+        #u_tiltcorr=np.real(u_complex)
+        #v_tiltcorr=np.imag(u_complex)				
+
+    def tilt_coords_wght(self):
+        """ Tilt the coordinate system so that the x-axis is always parallel to
+        the local mean wind direction. The y-axis stays horizontal while being 
+        perpendicular to the x-axis while the z-axis is perpendicular to the 
+        tilted x- and the tilted y-axis."""
+        
+        u_mean=self.weighted_component_mean[0]
+        v_mean=self.weighted_component_mean[1]
+        self.tilt_angle_deg=np.arctan2(v_mean,u_mean)*180/np.pi        
+
+        self.u_cart = self.u.copy()
+        self.v_cart = self.v.copy()
+        
+        fac1=1/np.sqrt(1+v_mean**2/u_mean**2)
+        
+        u_tiltcorr = self.u*fac1 + self.v*fac1*v_mean/u_mean
+        v_tiltcorr = -self.u*fac1*v_mean/u_mean + self.v*fac1
+        
+        self.u=u_tiltcorr
+        self.v=v_tiltcorr
+        
     def calc_magnitude(self):
         """ Calculate wind magnitude from components. """
         self.magnitude = np.sqrt(self.u**2 + self.v**2)
@@ -223,12 +343,13 @@ class Timeseries(pd.DataFrame):
         data in the BSA software. Transit time weighting removes a possible
         bias towards higher wind velocities. Returns the weighted u and v
         component means."""
-        
+        #edit 07/04/19: change np.isnan tp ~np.isnan, otherwise program works only with nan values
+            
         self.weighted_u_mean = wt.transit_time_weighted_mean(
-                                                        self.t_transit,
+                                                        self.t_transit[~np.isnan(self.t_transit)],
                                                         self.u.dropna().values)
         self.weighted_v_mean = wt.transit_time_weighted_mean(
-                                                        self.t_transit,
+                                                        self.t_transit[~np.isnan(self.t_transit)],
                                                         self.v.dropna().values)
 
         return float(self.weighted_u_mean),float(self.weighted_v_mean)
@@ -240,12 +361,13 @@ class Timeseries(pd.DataFrame):
         data in the BSA software. Transit time weighting removes a possible
         bias towards higher wind velocities. Returns the weighted u and v
         component variance."""
+        #edit 07/04/19: change np.isnan tp ~np.isnan, otherwise program works only with nan values
 
         self.weighted_u_var = wt.transit_time_weighted_var(
-                                                        self.t_transit,
+                                                        self.t_transit[~np.isnan(self.t_transit)],
                                                         self.u.dropna().values)
         self.weighted_v_var = wt.transit_time_weighted_var(
-                                                        self.t_transit,
+                                                        self.t_transit[~np.isnan(self.t_transit)],
                                                         self.v.dropna().values)
 
         return float(self.weighted_u_var),float(self.weighted_v_var)

windtunnel/timeseries_nc.py
@@ -28,10 +28,10 @@ class Timeseries_nc(pd.DataFrame):
         """ Initialise Timerseries_nc() object. """
         super().__init__()
 
-        self['t_arr_1'] = pd.Series(data=t_arr_1)
-        self['t_arr_2'] = pd.Series(data=t_arr_2)                                  
-        self['comp_1'] = pd.Series(data=comp_1,index = self['t_arr_1'])
-        self['comp_2'] = pd.Series(data=comp_2,index = self['t_arr_2'])
+        self.t_arr_1 = t_arr_1
+        self.t_arr_2 = t_arr_2
+        self.comp_1 = pd.Series(data=comp_1,index = t_arr_1)
+        self.comp_2 = pd.Series(data=comp_2,index = t_arr_2)
         
         self.x = x
         self.y = y
@@ -76,6 +76,12 @@ class Timeseries_nc(pd.DataFrame):
                                                   usecols=(1,2,3,4,5,6),
                                                   skip_header=6,unpack=True)
 
+        t_arr_1 = np.trim_zeros(t_arr_1,'b')
+        comp_1 = np.trim_zeros(comp_1, 'b')
+        t_transit_1 = np.trim_zeros(t_transit_1,'b')
+        t_arr_2 = np.trim_zeros(t_arr_2,'b')
+        comp_2 = np.trim_zeros(comp_2, 'b')
+        t_transit_2 = np.trim_zeros(t_transit_2,'b')
         return cls(comp_1,comp_2,x,y,z,t_arr_1,t_transit_1,t_arr_2,t_transit_2)
 
 
@@ -104,10 +110,15 @@ class Timeseries_nc(pd.DataFrame):
         """ Get wind components from filename.
         @parameter: filename, type = str """
         with open(filename) as file:
-            for i, line in enumerate(file):
-                if i == 5:
-                    self.wind_comp1 = line.split()[-4][-1].lower()
-                    self.wind_comp2 = line.split()[-2][-1].lower()
+            name = filename.split('/',-1)[-1]
+            if name.find('_UV_')>0:
+                pos = name.find('_UV_')
+            elif name.find('_UW_')>0:
+                pos = name.find('_UW_')
+            elif name.find('_VW_')>0:
+                pos = name.find('_VW_')
+            self.wind_comp_1 = name[pos+1].lower()
+            self.wind_comp_2 = name[pos+2].lower()
 
     def nondimensionalise(self):
         """ Nondimensionalise the data. wtref is set to 1 if no wtref is
@@ -127,8 +138,9 @@ class Timeseries_nc(pd.DataFrame):
         self.x = self.x * self.scale/1000           #[m]
         self.y = self.y * self.scale/1000           #[m]
         self.z = self.z * self.scale/1000           #[m]
-        self.t_arr = self.t_arr * self.scale/1000   #[s]
-        
+        self.t_arr_1 = self.t_arr_1 * self.scale / 1000   #[s]
+        self.t_arr_2 = self.t_arr_2 * self.scale / 1000  # [s]
+
     def pair_components(self,atol=1):
         """ Pair components in comp_1 and comp_2 using atol as absolute
         tolerance to match a pair of measurements. atol is set to 1 as default,
@@ -142,13 +154,15 @@ class Timeseries_nc(pd.DataFrame):
 
         self.paired_components = np.transpose(np.vstack([tmp_1,tmp_2]))
 
-    def equidistant(self):
+    def calc_equidistant_timesteps(self):
         """ Create equidistant time series. """
-        self.t_eq = np.linspace(self.t_arr[0],self.t_arr[-1],len(self.t_arr))
-        self.comp_1[:] = wt.equ_dist_ts(self.t_arr,self.t_eq,self.comp_1)
-        self.comp_2[:] = wt.equ_dist_ts(self.t_arr,self.t_eq,self.comp_2)
+        self.t_eq_1 = np.linspace(self.t_arr_1[0], self.t_arr_1[-1], len(self.t_arr_1))
+        self.t_eq_2 = np.linspace(self.t_arr_2[0], self.t_arr_2[-1], len(self.t_arr_2))
+        self.comp_1[:] = wt.equ_dist_ts(self.t_arr_1,self.t_eq_1,self.comp_1)
+        self.comp_2[:] = wt.equ_dist_ts(self.t_arr_2,self.t_eq_2,self.comp_2)
 
-        self.index = self.t_eq
+        self.index_1 = self.t_eq_1
+        self.index_2 = self.t_eq_2
         
     def mask_outliers(self,std_mask=5.):
         """ Mask outliers and print number of outliers. std_mask specifies the
@@ -161,10 +175,17 @@ class Timeseries_nc(pd.DataFrame):
         # Mask outliers
         u_mask = self.comp_1<(std_mask*np.std(self.comp_1)+np.mean(self.comp_1))
         v_mask = self.comp_2<(std_mask*np.std(self.comp_2)+np.mean(self.comp_2))
-        mask = np.logical_and(u_mask, v_mask)
 
-        self.comp_1 = self.comp_1[mask]
-        self.comp_2 = self.comp_2[mask]
+
+        self.comp_1 = self.comp_1[u_mask]
+        self.t_arr_1 = self.t_arr_1[u_mask]
+        self.t_transit_1 = self.t_transit_1[u_mask]
+
+
+        self.comp_2 = self.comp_2[v_mask]
+        self.t_arr_2 = self.t_arr_2[v_mask]
+        self.t_transit_2 = self.t_transit_2[v_mask]
+
 
         # Log outliers in console and to file
         logger.info('Outliers component 1: {} or {:.4f}%'.format(
@@ -204,9 +225,9 @@ class Timeseries_nc(pd.DataFrame):
         component means."""
         
         self.weighted_u_mean = wt.transit_time_weighted_mean(
-                                                        self.t_transit,self.u)
+                                                        self.t_transit_1,self.comp_1)
         self.weighted_v_mean = wt.transit_time_weighted_mean(
-                                                        self.t_transit,self.v)
+                                                        self.t_transit_2,self.comp_2)
 
         return float(self.weighted_u_mean), float(self.weighted_v_mean)
 
@@ -219,35 +240,35 @@ class Timeseries_nc(pd.DataFrame):
         component variance."""
 
         self.weighted_u_var = wt.transit_time_weighted_var(
-                                                        self.t_transit,self.u)
+                                                        self.t_transit_1,self.comp_1)
         self.weighted_v_var = wt.transit_time_weighted_var(
-                                                        self.t_transit,self.v)
+                                                        self.t_transit_2,self.comp_2)
 
         return float(self.weighted_u_var), float(self.weighted_v_var)
 
     @property
     def mean_magnitude(self):
         """ Calculate mean wind magnitude from unweighted components. """
-        if self.magnitude is None:
-            self.calc_magnitude()
-
-        return np.mean(self.magnitude)
-
+        #if self.magnitude is None:
+        #    self.calc_magnitude()
+        #
+        #return np.mean(self.magnitude)
+        return 0
     @property
     def mean_direction(self):
         """ Calculate mean wind direction from components relative to the wind
         tunnels axis."""
-        if self.paired_components is None:
-            self.pair_components()
-            print('Pairing components before calculation!')
+        #if self.paired_components is None:
+        #    self.pair_components()
+        #    print('Pairing components before calculation!')
         
-        unit_WD = np.arctan2(np.mean(self.paired_components[0]),
-                             np.mean(self.paired_components[1]))*\
-                             180/np.pi
-        mean_direction = (360 + unit_WD) % 360
-
-        return mean_direction
-
+        #unit_WD = np.arctan2(np.mean(self.paired_components[0]),
+        #                     np.mean(self.paired_components[1]))*\
+        #                     180/np.pi
+        #mean_direction = (360 + unit_WD) % 360
+        #
+        #return mean_direction
+        return 0
     def save2file(self,filename,out_dir=None):
         """ Save data from Timeseries object to txt file. filename must include
         '.txt' ending. If no out_dir directory is provided './' is set as