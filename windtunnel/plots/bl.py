Bug fixes. Calculation method for alpha to be verified!

windtunnel/plots/bl.py
@@ -343,7 +343,7 @@ def plot_winddata(mean_magnitude, u_mean, v_mean, heights, yerr=0, var_lat=None,
     v_mean = np.asarray(v_mean)
     heights = np.asarray(heights)
     
-    ret = []
+    ret = []  
     for i in range(np.size(mean_magnitude)):
         if lat == False:
             M = ax.errorbar(mean_magnitude[i],heights[i],yerr=yerr,marker='s',
@@ -381,7 +381,7 @@ def plot_winddata(mean_magnitude, u_mean, v_mean, heights, yerr=0, var_lat=None,
             ax.set_ylim(-0.1,0.7)
     
             ret.append(M + U + V)
-    
+    print(lgd)
     return ret, lgd
 
 def plot_winddata_log(mean_magnitude,u_mean,v_mean,heights,yerr=0,ax=None,

windtunnel/utils.py
@@ -457,18 +457,55 @@ def get_percentiles(data_dict, percentile_list):
 
     return percentile_dict
 	
-def calc_profile(mean_mag, heights, wtref, z_ref, d_0=0):
-    """Calculate profile exponent α and roughness length z_0"""
-    print("Warning: Assuming that wind data is non-dimensional, and that heights are full-scale.")
+def calc_profile(mean_mag, heights, wtref, z_ref, d_0=0, mode='all',min_height=None,max_height=None,split_height=None):
+    """Calculate profile exponent α and roughness length z_0, and save data to excel file. 
+    Avaliable modes are 'all' (calculates profile between min_height and max_height), and 
+    'seperate', which calulates two profiles, one above and one below split_height.
+    Setting minimum and maximum height to 'None' calculates profile to bottom and top of
+    data respectively. All heights assumed in m full-scale."""
+    print("Warning: Assuming that wind data is non-dimensional, and that heights are in m full-scale. d_0 in mm.")
     #Note: heights should be in (m) full-scale. Change code if this is not the case! 
-    z_norm = (np.array(heights)-d_0)/(z_ref-d_0)
-    #Note: the wind data in this script is already non-dimensional.
-    #If the code is edited to allow for dimensional plotting, change 
-    #function to non-dimensionalise data if necessary!
+    
+    heights=np.asarray(heights)
+    mean_mag=np.asarray(mean_mag)
+    if min_height == None:
+       min_height = np.min(heights)
+    if max_height == None:
+       max_height = np.max(heights)
+    if mode == 'all':
+       heights_mask=(heights>=min_height) & (heights<=max_height)
+       heights=heights[heights_mask]
+       mean_mag=mean_mag[heights_mask]
+       z_norm = (np.array(heights)-d_0)/(z_ref-d_0)
+       #Note: the wind data in this script is already non-dimensional.
+       #If the code is edited to allow for dimensional plotting, change 
+       #function to non-dimensionalise data if necessary!
+       z_norm=np.vstack([np.log(z_norm),np.zeros(len(z_norm))]).transpose()
+       print(z_norm)
+       alpha=np.linalg.lstsq(z_norm,np.log(np.array(mean_mag)))
+       print(alpha) 
+       return alpha
+    elif mode == 'seperate':
+       heights_bottom=heights[(heights>=min_height) & (heights<=split_height)]
+       mean_mag_bottom = mean_mag[(heights>=min_height) & (heights<=split_height)]
+       z_norm_bottom = (np.array(heights_bottom)-d_0)/(z_ref-d_0)
+       #Note: the wind data in this script is already non-dimensional.
+       #If the code is edited to allow for dimensional plotting, change 
+       #function to non-dimensionalise data if necessary!
+       z_norm_bottom=np.vstack([np.log(z_norm_bottom),np.zeros(len(z_norm_bottom))]).transpose()
+       print(z_norm_bottom)
+       alpha_bottom=np.linalg.lstsq(z_norm_bottom,np.log(np.array(mean_mag_bottom)))
+       print(alpha_bottom)
+       heights_top=heights[(heights>=split_height) & (heights<=max_height)]
+       mean_mag_top = mean_mag[(heights>=split_height) & (heights<=max_height)]
+       z_norm_top = (np.array(heights_top)-d_0)/(z_ref-d_0)
+       #Note: the wind data in this script is already non-dimensional.
+       #If the code is edited to allow for dimensional plotting, change 
+       #function to non-dimensionalise data if necessary!
+       z_norm_top=np.vstack([np.log(z_norm_top),np.zeros(len(z_norm_top))]).transpose()
+       print(z_norm_top)
+       alpha_top=np.linalg.lstsq(z_norm_top,np.log(np.array(mean_mag_top)))
+       print(alpha_top)  
+       return alpha_top       
 
-    z_norm=np.vstack([np.log(z_norm),np.zeros(len(z_norm))]).transpose()
-    print(z_norm)
-    alpha=np.linalg.lstsq(z_norm,np.log(np.array(mean_mag)))
-    print(alpha)
 
-    return alpha